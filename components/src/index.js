93c75e6ff6f1aece05afb308f8fd599b9cfbe9f0
binruli <lbycloud@gmail.com>
2021-06-07 16:00:03 -0500

    Specifying images in JSX

--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () =>{
     return (
-    <div className="ui container comments">
-        <div className="comments">
-            <a href="/" className="avatar">
-                <img alt="avatar" />
-            </a>
-            <div className="content">
+        <div className="ui container comments">
+            <div className="comments">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
                 <a href="/" className="author">
                     Sam
                 </a>
@@ -16,9 +17,9 @@ const App = () =>{
                     <span className="date">Today at 6:00PM</span>
                 </div>
                 <div classNmae="text">Nice blog post!</div>
-            </div>
-        </div > 
-    </div>
+                </div>
+            </div > 
+        </div>
 
     
     );
