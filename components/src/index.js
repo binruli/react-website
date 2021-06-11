7d3cd932f0df68482cd40928750b6aa722697ae2
binruli <lbycloud@gmail.com>
2021-06-11 17:01:30 -0500

    Showing custom childen

--- components/src/index.js
+++ components/src/index.js
@@ -2,14 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail'; // './' look in the same folder
-
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 6:00pm" content="Thank you for your sharing." avatar={faker.image.avatar()}/>
-            <CommentDetail author="Mary" timeAgo="Today at 7:00pm" content="Good job!" avatar={faker.image.avatar()}/>
-            <CommentDetail author="Alex" timeAgo="Yesterday at 8:00pm" content="Nice blog post!" avatar={faker.image.avatar()}/>
+            <ApprovalCard>
+                <div>
+                    <h4>Warning</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 6:00pm" content="Thank you for your sharing." avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Mary" timeAgo="Today at 7:00pm" content="Good job!" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Yesterday at 8:00pm" content="Nice blog post!" avatar={faker.image.avatar()}/>
+            </ApprovalCard>
         </div>
 
 
